2598435c2d03f9b1d03228718923af099893d179
gvenkatasudharshan <venkatasudharshangangula@gmail.com>
2023-12-03 16:32:16 +0530

    String program added

---
+++ Example/src/code/Stringequla.java
@@ -0,0 +1,15 @@
+package code;
+
+public class Stringequla 
+{
+	
+	static String a ="sudha";
+	static  String b= "Sudha";
+	
+	public static void main(String args[])
+	{
+		System.out.println(a.equals(b));
+		System.out.println(a.equalsIgnoreCase(b));
+	}
+
+}
