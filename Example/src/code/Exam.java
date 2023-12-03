8946f92622c32f70497b23dd87a1cb52fcb77eb3
gvenkatasudharshan <venkatasudharshangangula@gmail.com>
2023-12-03 15:28:00 +0530

    for loop program

--- Example/src/code/Exam.java
+++ Example/src/code/Exam.java
@@ -4,7 +4,9 @@ public class Exam
 {
   public static void main(String[] args)
   {
-	System.out.println("Hello world");
-	System.out.println("what");
+	for(int i=0;i<=5;i++)
+	{
+		System.out.println("GVk");
+	}
 }
 }
